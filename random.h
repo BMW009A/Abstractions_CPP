b2f635ec4f017a9f50750d29b040103417560620
Qiu Chaozhi <qiuchaozhi@outlook.com>
2024-08-17 19:43:15 +0900

    Initial commit

---
+++ random.h
@@ -0,0 +1,16 @@
+//
+// Created by QCZ on 8/17/2024.
+//
+
+#ifndef CS106B_RANDOM_H
+#define CS106B_RANDOM_H
+
+int randomInteger(int low, int high);
+
+double randomReal(double low, double high);
+
+bool randomChance(double p);
+
+void setRandomSeed(int seed);
+
+#endif //CS106B_RANDOM_H
